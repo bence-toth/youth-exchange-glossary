9aae5f1eb6a99faa5122e68e8e77ba68e417283d
Bence A. Tóth <5611525+bence-toth@users.noreply.github.com>
2022-10-19 14:42:14 +0200

    Seed code

---
+++ glossary.js
@@ -0,0 +1,21 @@
+const glossary = [
+    {
+        term: {
+            en: "gender identity",
+            pt: "identidade de género"
+        },
+        description: {
+            en: "something about gender identity",
+            pt: "something about gender identity but in portuguese"
+        }
+    }
+]
+
+const language = "en"
+
+document.getElementById("glossary").innerHTML = glossary
+    .sort((a, b) => a.term[language] < b.term[language])
+    .map(item => `
+        <dd>${item.term[language]}</dd>
+        <dt>${item.description[language]}</dt>
+    `)
